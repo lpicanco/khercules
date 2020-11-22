32875b5e9f0a81ddf5ca4b6c18e72ab48228d173
Luiz Picanço <lpicanco@gmail.com>
2020-11-22 15:04:18 -0300

    refactor: remove KVCollectionManagerIntegrationTest duplicate code

--- src/jvmTest/kotlin/com/khercules/KVCollectionManagerIntegrationTest.kt
+++ src/jvmTest/kotlin/com/khercules/KVCollectionManagerIntegrationTest.kt
@@ -4,16 +4,17 @@ import kotlin.test.assertEquals
 import org.junit.Test
 
 internal class KVCollectionManagerIntegrationTest {
+    private val firstSegmentFirstValue = "firstSegmentFirstValue".padEnd(200, '0')
+    private val firstSegmentLastValue = "firstSegmentLastValue".padEnd(200, '1')
+    private val secondSegmentFirstValue = "secondSegmentFirstValue".padEnd(402, '2')
+    private val thirdSegmentFirstValue = "thirdSegmentFirstValue".padEnd(200, '3')
+
     @Test
     fun testSegments() {
         val config = Config(
             databaseLocation = createTempDir().toPath().toString(),
             segmentSizeInBytes = 400
         )
-        val firstSegmentFirstValue = "firstSegmentFirstValue".padEnd(200, '0')
-        val firstSegmentLastValue = "firstSegmentLastValue".padEnd(200, '1')
-        val secondSegmentFirstValue = "secondSegmentFirstValue".padEnd(402, '2')
-        val thirdSegmentFirstValue = "thirdSegmentFirstValue".padEnd(200, '3')
 
         KVCollection(config).use { collection ->
             collection.put("0101", firstSegmentFirstValue.toByteArray())
@@ -21,18 +22,19 @@ internal class KVCollectionManagerIntegrationTest {
             collection.put("0201", secondSegmentFirstValue.toByteArray())
             collection.put("0301", thirdSegmentFirstValue.toByteArray())
 
-            assertEquals(firstSegmentFirstValue, collection.get("0101")?.decodeToString())
-            assertEquals(firstSegmentLastValue, collection.get("0102")?.decodeToString())
-            assertEquals(secondSegmentFirstValue, collection.get("0201")?.decodeToString())
-            assertEquals(thirdSegmentFirstValue, collection.get("0301")?.decodeToString())
+            verifyCollection(collection)
         }
 
-        // Tests open/close
+        // Tests reopening
         KVCollection(config).use { collection ->
-            assertEquals(firstSegmentFirstValue, collection.get("0101")?.decodeToString())
-            assertEquals(firstSegmentLastValue, collection.get("0102")?.decodeToString())
-            assertEquals(secondSegmentFirstValue, collection.get("0201")?.decodeToString())
-            assertEquals(thirdSegmentFirstValue, collection.get("0301")?.decodeToString())
+            verifyCollection(collection)
         }
+    }
+
+    private fun verifyCollection(collection: KVCollection) {
+        assertEquals(firstSegmentFirstValue, collection.get("0101")?.decodeToString())
+        assertEquals(firstSegmentLastValue, collection.get("0102")?.decodeToString())
+        assertEquals(secondSegmentFirstValue, collection.get("0201")?.decodeToString())
+        assertEquals(thirdSegmentFirstValue, collection.get("0301")?.decodeToString())
     }
 }
